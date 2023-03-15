9c25c3519dfe19eae561d463c27d4a7486a24a3d
Smuthukamatchi <85733169+Smuthukamatchi@users.noreply.github.com>
2023-03-15 11:32:28 +0530

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing.Matching;
+
+namespace PayrollApp.Models
+{
+    public partial class Admin
+    {
+        public Int16 Id { get; set; } = default!;
+        public string? Firstname { get; set; } = null!;
+        public string? Lastname { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public string? Password { get; set; } = null!;
+        
+    }
+}
---
+++ DBpayrollContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PayrollApp.Models
+{
+    public partial class DBpayrollContext : DbContext
+    {
+        public DBpayrollContext()
+        {
+        }
+
+        public DBpayrollContext(DbContextOptions<DBpayrollContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admins { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                 optionsBuilder.UseSqlServer("Server=DESKTOP-TCSN84U\\SQLEXPRESS;Database=DBpayroll;Trusted_Connection=True;");
+             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("admin");
+                entity.Property(e => e.Firstname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
